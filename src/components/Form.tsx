b901d7a56d8a2faffbe883a8336d5f66838ad521
Kadu <kadu.silva2014@gmail.com>
2024-07-23 18:41:50 -0300

    feat: Integra react-hook-form e Zod no Form
    Convertido o componente Form para utilizar react-hook-form para manipulação e validação de formulários, juntamente com um esquema Zod. Integrada funcionalidade para mascarar o campo de CEP e preencher automaticamente o endereço com a resposta da API ViaCEP. Atualizados os inputs do formulário para se registrarem com a função register do useForm. Removidos imports não utilizados e atualizadas dependências.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,39 +1,106 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import ErrorMessageForm from "./ErrorMessageForm";
 import InputPassword from "./InputPassword";
+import { withHookFormMask } from "use-mask-input";
+import axios from "axios";
+
+
+const formSchema = z.object({
+  name: z.string().min(3).max(255),
+  email: z.string().email(),
+  password: z.string().min(8),
+  confirmPassword: z.string().min(8),
+  terms: z.boolean(),
+  phone: z.string().max(20),
+  cpf: z.string().max(14),
+  zipCode: z.string().max(9),
+  address: z.object({
+    address: z.string().max(255),
+    city: z.string().max(255),
+  }),
+  city: z.string().max(255),
+})
+
+export type FormSchema = z.infer<typeof formSchema>
+
+export interface responseZipCode {
+  cep: string
+  logradouro: string
+  complemento: string
+  unidade: string
+  bairro: string
+  localidade: string
+  uf: string
+  ibge: string
+  gia: string
+  ddd: string
+  siafi: string
+}
+
 
 export default function Form() {
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+    setValue
+  } = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+  })
+
+  let zipCodeMessageError: string | undefined
+
+  const zipCodeWithMask = withHookFormMask(register('zipCode'), '99999-999', {
+    async oncomplete() {
+      const { data, status } = await axios.get<responseZipCode>(`https://viacep.com.br/ws/${watch('zipCode')}/json/`)
+      if (status !== 200) return zipCodeMessageError = 'CEP não encontrado'
+      zipCodeMessageError = undefined
+      setValue('address.city', data.localidade)
+      console.log("🚀 ~ oncomplete ~ data:", data)
+      setValue('address.address', data.logradouro || 'Sem dados')
+    },
+  })
+
+  const onSubmit = handleSubmit((data) => {
+    console.log("🚀 ~ onSubmit ~ data:", data)
+  })
+
 
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="mb-4">
         <label htmlFor="name">Nome Completo</label>
-        <input type="text" id="name" />
-        <ErrorMessageForm />
+        <input type="text" id="name" {...register('name')} />
+        <ErrorMessageForm message={errors.name?.message} />
       </div>
       <div className="mb-4">
         <label htmlFor="email">E-mail</label>
-        <input className="" type="email" id="email" />
+        <input className="" type="email" id="email" {...register('email')} />
       </div>
       <div className="mb-4">
         <label htmlFor="password">Senha</label>
-        <InputPassword id="password" />
+        <InputPassword id="password" {...register('password')} />
       </div>
       <div className="mb-4">
         <label htmlFor="confirm-password">Confirmar Senha</label>
-        <InputPassword id="confirm-password" />
+        <InputPassword id="confirm-password" {...register('confirmPassword')} />
       </div>
       <div className="mb-4">
         <label htmlFor="phone">Telefone Celular</label>
-        <input type="text" id="phone" />
+        <input type="text" id="phone" {...register('phone')} />
       </div>
       <div className="mb-4">
         <label htmlFor="cpf">CPF</label>
-        <input type="text" id="cpf" />
+        <input type="text" id="cpf" {...register('cpf')} />
       </div>
       <div className="mb-4">
         <label htmlFor="cep">CEP</label>
-        <input type="text" id="cep" />
+        <input type="text" id="cep" {...zipCodeWithMask} />
+        <ErrorMessageForm message={zipCodeMessageError} />
       </div>
       <div className="mb-4">
         <label htmlFor="address">Endereço</label>
@@ -42,6 +109,8 @@ export default function Form() {
           type="text"
           id="address"
           disabled
+
+          {...register('address.address')}
         />
       </div>
 
@@ -52,11 +121,13 @@ export default function Form() {
           type="text"
           id="city"
           disabled
+
+          {...register('address.city')}
         />
       </div>
       {/* terms and conditions input */}
       <div className="mb-4">
-        <input type="checkbox" id="terms" className="mr-2 accent-slate-500" />
+        <input type="checkbox" id="terms" className="mr-2 accent-slate-500" {...register('terms')} />
         <label
           className="text-sm  font-light text-slate-500 mb-1 inline"
           htmlFor="terms"
